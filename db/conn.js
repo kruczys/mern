d9c0f043d5e439302202a2ffa29afe053fca0802
Konrad Kreczko <konradkreczko@gmail.com>
2023-11-13 14:30:06 +0100

    Created basic structure

---
+++ db/conn.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require("mongodb");
+const Db = process.env.MONGO_URI;
+console.log(Db)
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var _db;
+
+module.exports = {
+    connectToServer: callback => {
+        client.connect((err, db) => {
+            if (db) {
+                _db = db.db("products");
+                console.log("Successfully connected to MongoDB.");
+            }
+            return callback(err);
+        });
+    },
+
+    getDb: () => {
+        return _db;
+    },
+};
---
+++ routes/record.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const recordRoutes = express.Router();
+
+const dbo = require("../db/conn");
+
+const ObjectId = require("mongodb").ObjectId;
